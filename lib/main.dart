ba0beb2cfb7fa3314b3998baff288fa6dfbf028d
MohsenEMX <zmc.site@gmail.com>
2023-06-01 11:42:09 +0330

    a lil code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors, curly_braces_in_flow_control_structures, unnecessary_brace_in_string_interps, avoid_print, await_only_futures, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
-import 'package:webview_flutter_plus/webview_flutter_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'webview.dart';
 
 // Used as settings
 String? hubip;
@@ -51,6 +51,7 @@ void skipIp(context, skiptowebapp) async {
       context,
       MaterialPageRoute(builder: (context) => const WebApp()),
     );*/
+    print('well shit this doesn\'t work');
   }
 }
 
@@ -218,75 +219,5 @@ class _HomePageState extends State<HomePage> {
         )
       ],
     )));
-  }
-}
-
-class WebApp extends StatefulWidget {
-  const WebApp({super.key});
-
-  @override
-  State<WebApp> createState() => _WebAppState();
-}
-
-class _WebAppState extends State<WebApp> {
-  @override
-  Widget build(BuildContext context) {
-    String? httpState;
-    if (isHTTPS == true)
-      httpState = "https";
-    else
-      httpState = "http";
-
-    if (hubport!.isEmpty || hubport == null) {
-      if (httpState == "http")
-        hubport = '80';
-      else if (hubport == "https") hubport = '443';
-    }
-
-    hubip = hubip!.trim();
-    setState(() {
-      showIPError = false;
-      showPortError = false;
-    });
-
-    return WillPopScope(
-        onWillPop: () async {
-          ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Back Button is disabled')));
-          return false;
-        },
-        child: Scaffold(
-            body: SafeArea(
-                child: WebViewPlus(
-          initialUrl: '${httpState}://${hubip}:${hubport}',
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebResourceError: (error) async {
-            print(await error.description);
-            print(await error.failingUrl);
-            int failingPort = Uri.parse(await error.failingUrl!).port;
-            if (await error.description == 'net::ERR_UNSAFE_PORT') {
-              setState(() {
-                showPortError = true;
-                portFieldErrorText =
-                    'The selected port is unsafe. Please change the port.';
-              });
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
-              );
-            } else if (failingPort != 703 &&
-                await error.description == 'net::ERR_NAME_NOT_RESOLVED') {
-              setState(() {
-                showIPError = true;
-                ipFieldErrorText =
-                    'Hub is unreachable. Please try double checking the IP and Port.';
-              });
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
-              );
-            }
-          },
-        ))));
   }
 }
